79629bb7eafb8b796c84fbc05cad0013ebdbb378
Alexander Shiryaev <alex_sh_v@mail.ru>
2021-06-11 10:53:45 +0300

    sidebar finished

--- sidebar/src/App.js
+++ sidebar/src/App.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import Products from './pages/Products';
+import Reports from './pages/Reports';
+import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      Sidebar
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path='/' exact component={Home} />
+          <Route path='/reports' component={Reports} />
+          <Route path='/products' component={Products} />
+        </Switch>
+      </Router>
+    </>
   );
 }
 
---
+++ sidebar/src/pages/Reports.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Reports() {
+  return (
+    <div className='reports'>
+      <h2>Reports</h2>
+    </div>
+  );
+}
+
+export default Reports;
